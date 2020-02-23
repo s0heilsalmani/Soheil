04cafb290acfee44ffd27c7f4c5ea2c4d4a72f62
s0heilsalmani <46238672+s0heilsalmani@users.noreply.github.com>
2020-02-24 01:18:43 +0400

    Update side nav JS

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,8 @@
-function w3_open() {
-    document.getElementById("main").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
-  }
-  
-  function w3_close() {
-    document.getElementById("main").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
